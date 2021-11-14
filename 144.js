0086235d674e4bc50a41baa08c1d56c43892274c
Mina <2980090850@qq.com>
2021-11-14 22:39:51 +0800

    Add files via upload

---
+++ 144.js
@@ -0,0 +1,28 @@
+// 二叉树的前序遍历
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var preorderTraversal = function(root) {
+  if (!root) return [];
+  let res = [];
+  res.push(root.val);
+  res.push(...preorderTraversal(root.left));
+  res.push(...preorderTraversal(root.right));
+  return res;
+};
